28187b690336f977ab959125b1686eb510955b8e
NKompa <nkompa7@gmail.com>
2022-09-16 15:19:22 +0300

    Raise A to the B power

---
+++ Task04/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии: A = 3; B = 5 -> 243 (3⁵).
+
+int PowerNumber (int a, int b)
+{
+    if (b==0) return 1;
+    return a * PowerNumber (a,b-1);
+}
+Console.WriteLine(PowerNumber(2,3));
