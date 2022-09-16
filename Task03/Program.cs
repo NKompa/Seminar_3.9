39ea07c0ed99b89254d880ab6c89b335e5fb28f9
NKompa <nkompa7@gmail.com>
2022-09-16 15:15:16 +0300

    Sum of digits in number

---
+++ Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа принимает на вход число и возвращать сумму его цифр. 453 -> 12.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int n)
+{
+    if(n == 0) return 0;
+    return (n % 10 + SumNumber(n/10));
+}
+Console.WriteLine(SumNumber(number));
