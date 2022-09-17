9715deb6198f7bc20b12bfeb177266150b9264b2
NKompa <nkompa7@gmail.com>
2022-09-17 13:39:17 +0300

    Print numbers from 1 to N

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,8 +1,9 @@
 ﻿// Задайте значение N. Программа выводит все натуральные числа в промежутке от 1 до N: N = 5 -> "1, 2, 3, 4, 5".
 
 Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());  
 int start = 1;
+if (num<start) Console.WriteLine("Введено неверное число");
 
 string NumRange (int numStart,int numEnd)
 {
